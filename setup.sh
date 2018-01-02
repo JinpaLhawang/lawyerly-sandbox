cac18225bac51f2cd4284d3ecf36010cf92aca5a
Nicholas Eden-Walker <nedenwalker@kenzan.com>
2018-01-02 11:43:49 -0500

    Quote fix.

--- setup.sh
+++ setup.sh
@@ -10,18 +10,18 @@ echo "${yellow}---"
 echo "--- account-edge-jvm-mvn ---"
 echo "---${reset}"
 cd account-edge-jvm-mvn/
-echo "${cyan}Building Project and Generating Contracts where `account-edge-jvm-mvn` is a Consumer...${reset}"
+echo "${cyan}Building Project and Generating Contracts where 'account-edge-jvm-mvn' is a Consumer...${reset}"
 ./mvnw clean install
-echo "${cyan}Publishing Contracts where `account-edge-jvm-mvn` is a Consumer...${reset}"
+echo "${cyan}Publishing Contracts where 'account-edge-jvm-mvn' is a Consumer...${reset}"
 ./mvnw pact:publish
 
 echo "${yellow}---"
 echo "--- orders-edge-jvm ---"
 echo "---${reset}"
 cd ../orders-edge-jvm/
-echo "${cyan}Building Project and Generating Contracts where `orders-edge-jvm` is a Consumer...${reset}"
+echo "${cyan}Building Project and Generating Contracts where 'orders-edge-jvm' is a Consumer...${reset}"
 ./gradlew clean build
-echo "${cyan}Publishing Contracts where `orders-edge-jvm` is a Consumer...${reset}"
+echo "${cyan}Publishing Contracts where 'orders-edge-jvm' is a Consumer...${reset}"
 ./gradlew pactPublish
 
 echo "${yellow}---"
@@ -29,9 +29,9 @@ echo "--- public-ui-js ---"
 echo "---${reset}"
 cd ../public-ui-js/
 npm install
-echo "${cyan}Generating Contracts where `public-ui-js` is a Consumer...${reset}"
+echo "${cyan}Generating Contracts where 'public-ui-js' is a Consumer...${reset}"
 npm run test:pact:consumer
-echo "${cyan}Publishing Contracts where `public-ui-js` is a Consumer...${reset}"
+echo "${cyan}Publishing Contracts where 'public-ui-js' is a Consumer...${reset}"
 npm run test:pact:publish
 
 echo "${yellow}---"
@@ -39,9 +39,9 @@ echo "--- admin-ui-js ---"
 echo "---${reset}"
 cd ../admin-ui-js/
 npm install
-echo "${cyan}Generating Contracts where `admin-ui-js` is a Consumer...${reset}"
+echo "${cyan}Generating Contracts where 'admin-ui-js' is a Consumer...${reset}"
 npm run test:pact:consumer
-echo "${cyan}Publishing Contracts where `admin-ui-js` is a Consumer...${reset}"
+echo "${cyan}Publishing Contracts where 'admin-ui-js' is a Consumer...${reset}"
 npm run test:pact:publish
 
 echo "${cyan}Built and Published Contracts to Pact Broker at http://localhost.${reset}"
