3a9806297d680ebeaceb9878a25a94c669e7a045
Nicholas Eden-Walker <nedenwalker@kenzan.com>
2018-01-02 11:38:28 -0500

    Formatting.

--- setup.sh
+++ setup.sh
@@ -6,26 +6,34 @@ reset=`tput sgr0`
 
 echo "${cyan}Building and Publishing Contracts to Broker...${reset}"
 
-tput setaf 6; echo "${yellow}--- account-edge-jvm-mvn ---${reset}"
-cd $LAWYERLY_SANDBOX/account-edge-jvm-mvn/
+echo "${yellow}---"
+echo "--- account-edge-jvm-mvn ---"
+echo "---${reset}"
+cd account-edge-jvm-mvn/
 ./mvnw clean install
 ./mvnw pact:publish
 
-tput setaf 6; echo "${yellow}--- orders-edge-jvm ---${reset}"
-cd $LAWYERLY_SANDBOX/orders-edge-jvm/
+echo "${yellow}---"
+echo "--- orders-edge-jvm ---"
+echo "---${reset}"
+cd ../orders-edge-jvm/
 ./gradlew clean build
 ./gradlew pactPublish
 
-tput setaf 6; echo "${yellow}--- public-ui-js ---${reset}"
-cd $LAWYERLY_SANDBOX/public-ui-js/
+echo "${yellow}---"
+echo "--- public-ui-js ---"
+echo "---${reset}"
+cd ../public-ui-js/
 npm install
 npm run test:pact:consumer
 npm run test:pact:publish
 
-tput setaf 6; echo "${yellow}--- admin-ui-js ---${reset}"
-cd $LAWYERLY_SANDBOX/admin-ui-js/
+echo "${yellow}---"
+echo "--- admin-ui-js ---"
+echo "---${reset}"
+cd ../admin-ui-js/
 npm install
 npm run test:pact:consumer
 npm run test:pact:publish
 
-tput setaf 3; echo "${cyan}Built and Published Contracts to Pact Broker at http://localhost.${reset}"
+echo "${cyan}Built and Published Contracts to Pact Broker at http://localhost.${reset}"
