0f3abce29737e8424f4efde1fc6f2eb52f3145ff
Nicholas Eden-Walker <nedenwalker@kenzan.com>
2018-01-02 11:20:39 -0500

    Comment formatting.

--- setup.sh
+++ setup.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-tput setaf 3; echo "Building and Publishing Contracts to Broker..."
+yellow=`tput setaf 3`
+cyan=`tput setaf 6`
+reset=`tput sgr0`
 
-tput setaf 6; echo "--- account-edge-jvm-mvn ---"
+echo "${cyan}Building and Publishing Contracts to Broker...${reset}"
+
+tput setaf 6; echo "${yellow}--- account-edge-jvm-mvn ---${reset}"
 cd $LAWYERLY_SANDBOX/account-edge-jvm-mvn/
 ./mvnw clean install
 ./mvnw pact:publish
 
-tput setaf 6; echo "--- orders-edge-jvm ---"
+tput setaf 6; echo "${yellow}--- orders-edge-jvm ---${reset}"
 cd $LAWYERLY_SANDBOX/orders-edge-jvm/
 ./gradlew clean build
 ./gradlew pactPublish
 
-tput setaf 6; echo "--- public-ui-js ---"
+tput setaf 6; echo "${yellow}--- public-ui-js ---${reset}"
 cd $LAWYERLY_SANDBOX/public-ui-js/
 npm install
 npm run test:pact:consumer
 npm run test:pact:publish
 
-tput setaf 6; echo "--- admin-ui-js ---"
+tput setaf 6; echo "${yellow}--- admin-ui-js ---${reset}"
 cd $LAWYERLY_SANDBOX/admin-ui-js/
 npm install
 npm run test:pact:consumer
 npm run test:pact:publish
 
-tput setaf 3; echo "Built and Published Contracts to Pact Broker at http://localhost."
+tput setaf 3; echo "${cyan}Built and Published Contracts to Pact Broker at http://localhost.${reset}"
